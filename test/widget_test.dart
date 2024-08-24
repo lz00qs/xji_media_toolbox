62fde9488e4c9514fedc04865fa9546b1c34d462
lz00qs <lz00qs@gmail.com>
2024-08-24 17:41:12 +0800

    build: ignore widget_test error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:xji_footage_toolbox/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(isFFmpegAvailable: true));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
