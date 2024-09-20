6fa67ed090016b6c873af07746bd14f83850c803
lz00qs <lz00qs@gmail.com>
2024-09-20 22:06:27 +0800

    perf: rename the app

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"xji_footage_toolbox", origin, size)) {
+  if (!window.Create(L"XJI Media Toolbox", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
