ee5157e1fc6dc5d0497f8eedabe19e5eca69ac28
lz00qs <lz00qs@gmail.com>
2024-11-22 02:54:53 +0800

    bugfix: fix windows assets path issue

--- lib/utils/ffmpeg_utils.dart
+++ lib/utils/ffmpeg_utils.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class FFmpegUtils {
   static var ffmpeg = '';
   static var ffprobe = '';
@@ -18,11 +20,25 @@ class FFmpegUtils {
         return true;
       }
     } else if (Platform.isWindows) {
-      if (File('assets/windows/ffmpeg/ffmpeg.exe').existsSync() &&
-          File('assets/windows/ffmpeg/ffprobe.exe').existsSync()) {
-        ffmpeg = 'assets/windows/ffmpeg/ffmpeg.exe';
-        ffprobe = 'assets/windows/ffmpeg/ffprobe.exe';
-        return true;
+      if (kDebugMode) {
+        if (File('assets/windows/ffmpeg/ffmpeg.exe').existsSync() &&
+            File('assets/windows/ffmpeg/ffprobe.exe').existsSync()) {
+          ffmpeg = 'assets/windows/ffmpeg/ffmpeg.exe';
+          ffprobe = 'assets/windows/ffmpeg/ffprobe.exe';
+          return true;
+        }
+      } else {
+        String appPath = Platform.resolvedExecutable;
+        appPath = appPath.substring(0, appPath.lastIndexOf("\\"));
+        String assetsPath =
+            '$appPath\\data\\flutter_assets\\assets\\windows\\ffmpeg\\';
+        ffmpeg = '$assetsPath\\ffmpeg.exe';
+        ffprobe = '$assetsPath\\ffprobe.exe';
+        if (File(ffmpeg).existsSync() && File(ffprobe).existsSync()) {
+          return true;
+        }
+        ffmpeg = '';
+        ffprobe = '';
       }
     }
     return false;
